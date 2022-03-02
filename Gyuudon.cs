aa20e039b7a94aec169628b6622dd0c7169d2b81
lebrancconvas <lebrancconvas@gmail.com>
2022-03-02 18:46:10 +0700

    :truck: Rename Game1 to Project Name.

--- Gyuudon.cs
+++ Gyuudon.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace gyuudon
 {
-    public class Game1 : Game
+    public class Gyuudon : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        public Game1()
+        public Gyuudon()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace gyuudon
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Gyuudon())
                 game.Run();
         }
     }
